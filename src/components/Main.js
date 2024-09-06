acdedbb658a0c8c2613cbc5ff08d11593a91e2d0
Gonzalo <gje@mit.edu>
2024-09-05 21:49:44 -0400

    added github link, and link to software design website

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import './Main.css';
 import { useState } from 'react';
 
-const aboutMeString = "Hello! I'm Gonzalo Esparza a junior at the Massachussets Institute of Technology, studying Computer Science Engineering. I have a particular interest in full stack web development, game development, and machine learning";
+const aboutMeString = "Hello! I'm Gonzalo Esparza a junior at the Massachussets Institute of Technology, studying Computer Science Engineering. I have a particular interest in full stack web development, game development, and machine learning. I'm Eager to join the software engineering field and further develop my skills. Visit my github to see what I have been working on lately.";
 const currentCourseworkString = "Software Design, Introduction to Algorithms, Computation Structures, Introduction to Probability, Intro to Machine Learning";
 const pastCourseworkString = 'Introduction to Computer Science Programming in Python, Introduction to Computational Thinking and Data Science, Fundamentals of Programming, Software Construction, Introduction to Low-Level Programming, +General Institue Requirements'
 
@@ -19,6 +19,8 @@ function Main(props){
                     {isCollapsed ? 'Show Past Coursework' : 'Hide Past Coursework'}
                 </button>
                 {!isCollapsed && <p>{pastCourseworkString}</p>}
+            <h2>Visit my Software Design class web portfolio</h2>
+                <a href='https://gonzonk.github.io/portfolio-gje/'>Software Design Portfolio</a>
         </main>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ function Nav(props){
             <ul>
                 <li><a href={resume} download='GonzaloEsparzasResume'>Resume</a></li>
                 <li><a href="/personalWebsite">Home</a></li>
+                <li><a href='https://github.com/gonzonk'>Github</a></li>
             </ul>
         </nav>
     )
